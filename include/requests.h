a8f0ade635c94160edcfd6a920ce7bf27e91b261
Nartov Dima <nartov.dima2018@yandex.ru>
2023-03-12 16:01:25 +0300

    Fixed typo

--- include/requests.h
+++ include/requests.h
@@ -94,9 +94,9 @@ constexpr size_t calculate_size(const RequestType type)
     case RequestType::New:
         return 36 + new_order_bitfield_num() + new_order_opt_fields_size();
     case RequestType::NewCross:
-        return 55 + new_order_bitfield_num() + new_order_cross_opt_bitfield_size();
+        return 45 + new_order_cross_bitfield_num() + new_order_cross_opt_bitfield_size();
     case RequestType::NewCrossMultileg:
-        return 55 + new_order_bitfield_num() + new_order_cross_multileg_opt_bitfield_size();
+        return 45 + new_order_cross_multileg_bitfield_num() + new_order_cross_multileg_opt_bitfield_size();
     }
     return 0;
 }
